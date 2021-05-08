563344279f915832c84fa23fd43580a13fd36757
Mohammed Amine Saidi <mohammed-amine.saidi@etu.utc.fr>
2021-05-08 22:34:53 +0200

    User Auth.

--- My_Blog_Flask/__init__.py
+++ My_Blog_Flask/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9ee24808950fd24f818ba6ee3763d29c' #secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) 
 
 from My_Blog_Flask import routes
