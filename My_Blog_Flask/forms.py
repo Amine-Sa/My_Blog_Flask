1c86775fdc13885fd282c4be26edf433631762f0
Mohammed Amine Saidi <mohammed-amine.saidi@etu.utc.fr>
2021-05-08 22:40:23 +0200

    update User Auth.

--- My_Blog_Flask/forms.py
+++ My_Blog_Flask/forms.py
@@ -1,7 +1,8 @@
 # Doc : https://flask.palletsprojects.com/en/1.1.x/patterns/wtforms/
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from My_Blog_Flask.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -15,6 +16,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
