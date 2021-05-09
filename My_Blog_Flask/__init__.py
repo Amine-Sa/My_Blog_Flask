6bc20e4610317a0561134c6c5b517657357ef473
Mohammed Amine Saidi <mohammed-amine.saidi@etu.utc.fr>
2021-05-09 02:59:44 +0200

    update templates and requirements

--- My_Blog_Flask/__init__.py
+++ My_Blog_Flask/__init__.py
@@ -9,5 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app) 
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from My_Blog_Flask import routes
