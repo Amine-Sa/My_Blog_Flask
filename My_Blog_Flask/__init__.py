179f971fbd556b003b89699f405465c15e1eebe7
Mohammed Amine Saidi <mohammed-amine.saidi@etu.utc.fr>
2021-05-09 02:25:45 +0200

    Add login manager

--- My_Blog_Flask/__init__.py
+++ My_Blog_Flask/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9ee24808950fd24f818ba6ee3763d29c' #secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app) 
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app) 
 
 from My_Blog_Flask import routes
--- My_Blog_Flask/models.py
+++ My_Blog_Flask/models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
-from My_Blog_Flask import db
+from My_Blog_Flask import db, login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
